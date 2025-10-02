77f75027f3f232289dd69e901742722e2348c05c
Cesar Barrera <barreracesar169@gmail.com>
2025-10-02 00:14:01 -0600

    weeweedf

--- frontend/clickyfest-app/src/environments/environment.prod.ts
+++ frontend/clickyfest-app/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://clickyfest-api.azurewebsites.net/api' // URL para producción
+  // Usa el dominio real del App Service (el que indicaste): DNS regional v4
+  apiUrl: 'https://clickyfest-api-c4eug4chf8g6gwcz.canadacentral-01.azurewebsites.net/api'
 };
